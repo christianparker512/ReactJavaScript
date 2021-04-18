a49479449cd20149856eed4f07473ca7537f122f
Christian Parker <christianparker512@gmail.com>
2021-04-18 16:12:34 -0500

    Use of Const needs to be something that doesn't change

--- js/let.js
+++ js/let.js
@@ -1,5 +1,11 @@
+// Because this a const it's going to throw an error'
+const myName = 'Quincy';
+console.log(myName);
+
+
 let myName = "Chris";
 console.log(myName);
 
 myName = 'Lisa';
 console.log(myName);
+
