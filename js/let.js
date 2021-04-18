9e4e25dfa13651ba215ad5232dc7f89584216754
Christian Parker <christianparker512@gmail.com>
2021-04-18 17:15:54 -0500

    destructuring

--- js/let.js
+++ js/let.js
@@ -57,3 +57,27 @@ class Person extends Human{
 const person = new Person();
 person.printMyName();
 person.printGender();
+
+//rest and spread
+const numbers = [1,2,3];
+const newNumbers = [...numbers,4];
+console.log(newNumbers);
+
+const person1 = {
+  name: 'Chris'
+};
+const newPerson = {
+  ...person1,
+  age: 50
+}
+console.log(newPerson);
+
+//destructuring - easily extract array elements or object properties and store them in a variable
+//array destructuring
+[a,b]=['Hello', 'chris']
+console.log(a)//hello
+console.log(b)
+
+const numbersDestructuring =[1,2,3];
+[num1, ,num3] = numbersDestructuring;
+console.log(num1,num3);
