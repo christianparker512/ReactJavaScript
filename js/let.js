f4f2bccf52fdd5dcd92d3049fd5fb5b19b193430
Christian Parker <christianparker512@gmail.com>
2021-04-18 16:18:25 -0500

    const as a variable using the arrow

--- js/let.js
+++ js/let.js
@@ -1,11 +1,26 @@
 // Because this a const it's going to throw an error'
-const myName = 'Quincy';
-console.log(myName);
+// const myName = 'Quincy';
+// console.log(myName);
 
 
-let myName = "Chris";
-console.log(myName);
+// let myName = "Chris";
+// console.log(myName);
+//
+// myName = 'Lisa';
+// console.log(myName);
 
-myName = 'Lisa';
-console.log(myName);
+// function printMyName(name){
+//   console.log(name);
+// }
+// printMyName('Chris');
 
+// const printHerName(name){
+//   console.log(name);
+// }
+// printHerName('lisa');
+
+
+const printMyName = (name, age) => {
+  console.log(name, age);
+}
+printMyName('Chris', 50);
